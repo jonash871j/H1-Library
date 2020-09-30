81a22c0531a95c9d4deb78f994992feb6c7763ac
Jonas hofmann <Jonash871j@gmail.com>
2020-09-30 18:47:06 +0200

    Add project files.

---
+++ Bibliotek 25022020 CSharp/Book.cs
@@ -0,0 +1,73 @@
+﻿public enum BookGenre
+{
+    Fantasy,
+    Adventure,
+    Romance,
+    Contemporary,
+    Dystopian,
+    Mystery,
+    Horror,
+    Thriller,
+};
+
+class Book
+{
+    private string name;
+    private string language;
+    private BookGenre genre;
+    private string[] genreArray;
+    private int sites;
+
+    public string Name
+    {
+        get { return name; }
+    }
+    public string Language
+    {
+        get { return language; }
+    }
+    public BookGenre Genre
+    {
+        get { return genre; }
+    }
+    public int Sites
+    {
+        get { return sites; }
+    }
+
+    public Book(string name, string language, BookGenre genre, int sites)
+    {
+        this.name = name;
+        this.language = language;
+        this.genre = genre;
+        this.sites = sites;
+        genreArray = new string[]
+        {
+            "fantasy",
+            "adventure",
+            "romance",
+            "contemporary",
+            "dystopian",
+            "mystery",
+            "horror",
+            "Thriller",
+        };
+    }
+
+    public string GetGenre()
+    {
+        for (int i = 0; i < genreArray.Length; i++)
+            if (i == (int)genre)
+                return genreArray[i];
+        return "none";
+    }
+
+    public string GetInfomation()
+    {
+        return
+            "    book name : " + name + "\n" +
+            "book language : " + language + "\n" +
+            "   book genre : " + genre + "\n" +
+            "   book sites : " + sites + "\n";
+    }
+}
---
+++ Bibliotek 25022020 CSharp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void PrintBookStack(Stack<Book> bookStack)
+    {
+        int i = 0;
+        foreach (Book book in bookStack)
+        {
+            Console.WriteLine(i + ".\n" + book.GetInfomation());
+            i++;
+        }
+    }
+    static void PrintBookList(List<Book> bookList)
+    {
+        int i = 0;
+        foreach (Book book in bookList)
+        {
+            Console.WriteLine(i + ".\n" + book.GetInfomation());
+            i++;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Book book01 = new Book("A Book", "Danish", BookGenre.Adventure, 128);
+        Book book02 = new Book("Scary", "English", BookGenre.Horror, 166);
+        Book book03 = new Book("Dummy Book", "English", BookGenre.Fantasy, 10);
+
+        Stack<Book> user = new Stack<Book>();
+        List<Book> system = new List<Book>() { book01, book02, book03 };
+
+        while (true)
+        {
+            Console.WriteLine(" - Book System");
+            PrintBookList(system);
+
+            Console.WriteLine("\n - User");
+            PrintBookStack(user);
+
+            Console.WriteLine("-1 to put all book back.");
+            Console.Write("Loan choice: ");
+            
+            try
+            {
+                int choice = int.Parse(Console.ReadLine());
+
+                if (choice == -1)
+                {
+                    int length = user.Count;
+                    for (int i = 0; i < length; i++)
+                        system.Add(user.Pop());
+                }
+                else
+                {
+                    user.Push(system[choice]);
+                    system.RemoveAt(choice);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Failed...");
+                Console.ReadKey();
+            }
+            Console.Clear();
+        }
+    }
+}
